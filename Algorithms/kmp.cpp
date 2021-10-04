aa4496ed8e12b8b09765227d1bdfd471b479e029
Đỗ Quốc Việt <64247567+vietdoo@users.noreply.github.com>
2021-10-04 15:43:40 +0700

    kmp

---
+++ Algorithms/kmp.cpp
@@ -0,0 +1,39 @@
+//Author: vietdoo
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define pii pair <int, int>
+#define pb push_back
+#define int long long
+#define el '\n'
+#define len(a) a.length()
+
+const double pi = acos(-1.0);
+const int mod = 1000000007;
+const int oo = 2e10 + 9;
+const int N = 3e5 + 9;
+
+int t = 1, n, m, k;
+int a[N]; 
+
+void Main () {
+	cin >>
+
+}
+
+signed main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	#endif 
+	
+	cin >> t;
+	while (t--) {
+		Main();
+	}
+
+	return 0;
+}
